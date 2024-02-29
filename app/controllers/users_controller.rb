d851100e4a2cc17341463ab6fce80070015dd246
Go0107 <srfiveu.0107@gmail.com>
2024-02-29 20:09:29 +0900

    カラム名の変更、APIモードへの変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,26 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
+    render status: 200, json: { users: @users }
+  end
+
+  def create
+    @user = User.new(user_params)
+    # データベースに保存します
+    if @user.save
+      render json: { user: @user }, status: :created
+    else
+      render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
   end
 end
