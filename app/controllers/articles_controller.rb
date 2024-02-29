c3f12c2fff1c5b70c75fc310e3b7353ea7cdd50d
Go0107 <srfiveu.0107@gmail.com>
2024-03-01 04:33:06 +0900

    update article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:update, :destroy]
+
   def index
     @articles = Article.all
     render status: 200, json: { articles: @articles }
@@ -14,7 +16,19 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def update
+    if @article.update(article_params)
+      render json: { article: @article }, status: :created
+    else
+      render json: { errors: @article.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
   private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
 
   def article_params
     params.require(:article).permit(:title, :description, :body)
